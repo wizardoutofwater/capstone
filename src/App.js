1124662da6a2e408b1294934a34bb78009e6f2f4
Zach Smith <znathanielsmith@gmail.com>
2021-05-01 10:31:40 -0400

    fix sign up route and fix search query

--- src/App.js
+++ src/App.js
@@ -1,47 +1,44 @@
-import React, { useState, useEffect} from 'react';
-import { NavLink, Route, Switch } from 'react-router-dom';
-import './App.css';
-import Header from './components/Header';
-import  SignUp from './pages/SignUp';
-import  Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
-
+import React, { useState, useEffect } from "react";
+import { NavLink, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   const [token, setToken] = useState(null);
 
   useEffect(() => {
-    let token = localStorage.getItem('user-token');
+    let token = localStorage.getItem("user-token");
     if (token) {
       setToken(token);
     }
-  }, [])
+  }, []);
 
   return (
     <div className="App">
-    <Header token={token} />
-    <div className ="section main-content">
-    <Switch>
+      <Header token={token} />
+      <div className="section main-content">
+        <Switch>
           <Route exact path="/">
             {/* <Landing /> */}
             <h1>this will be the homePage</h1>
           </Route>
-          <Route path="/login" render={(props) => (
-            <Login {...props} updateToken={setToken} />
-          )}>
-          </Route>
-          <Route path="/signup">
-            <SignUp />
-          </Route>
+          <Route
+            path="/login"
+            render={(props) => <Login {...props} updateToken={setToken} />}
+          ></Route>
+          <Route
+            path="/signup"
+            render={(props) => <SignUp {...props} updateToken={setToken} />}
+          ></Route>
           <Route path="/dashboard">
-            <Dashboard token={token}/>
+            <Dashboard token={token} />
           </Route>
         </Switch>
-   
+      </div>
     </div>
-
-    </div>
-
   );
 }
 
