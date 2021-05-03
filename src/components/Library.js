7d6b05b8a2358d996fde994d0a70bbb120fe910f
Zach Smith <znathanielsmith@gmail.com>
2021-05-03 09:28:35 -0400

    finish search

--- src/components/Library.js
+++ src/components/Library.js
@@ -9,42 +9,44 @@ function Library(props) {
   const [snippetsResponse, setSnippetsResponse] = useState(null);
 
   useEffect(() => {
+    if (!props.token) return;
     const headers = {
       "Content-Type": "application/json",
       Authorization: `Bearer ${props.token}`,
     };
+    console.log(headers);
     axios.get("/api/user/snippets", { headers }).then((response) => {
       console.log(response);
       setSnippetsResponse(response.data.snippets);
     });
 
-    // empty dependency array means this effect will only run once (like componentDidMount in classes)
-  }, []);
+    // use effect runs before props passed the token on refresh, adding the props.token as a dependency ensures the call runs once props sends the token
+  }, [props.token]);
 
-  const renderSnippets= () => {
+  const renderSnippets = () => {
     if (
       !snippetsResponse ||
       snippetsResponse === null ||
       snippetsResponse === []
     ) {
-        return
-    //   return <div>No Snippets in Library</div>;
+      return;
+      //   return <div>No Snippets in Library</div>;
     }
     return (
       <>
         {snippetsResponse.map((snippet) => {
-            let languageName = languageAlias[snippet.language_id.toString()];
+          let languageName = languageAlias[snippet.language_id.toString()];
           return (
-            <Snippet 
-                key={snippet.id}
-                id={snippet.id}
-                title={snippet.title}
-                code={snippet.snippet}
-                note={snippet.note}
-                langId={snippet.language_id}
-                langName={languageName}
-                token={props.token}
-                />
+            <Snippet
+              key={snippet.id}
+              id={snippet.id}
+              title={snippet.title}
+              code={snippet.snippet}
+              note={snippet.note}
+              langId={snippet.language_id}
+              langName={languageName}
+              token={props.token}
+            />
           );
         })}
       </>
