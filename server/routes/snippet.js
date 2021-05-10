b57f3c58e57af28d9819b4d6d0c1f3bd86b496b2
Zach Smith <znathanielsmith@gmail.com>
2021-05-10 09:31:31 -0400

    joi validation

--- server/routes/snippet.js
+++ server/routes/snippet.js
@@ -3,8 +3,19 @@ require("dotenv").config();
 const express = require("express");
 const router = express.Router();
 const db = require("../models");
+const joi = require("joi");
 const authenticateToken = require("./helper/authenticateToken");
 router.use(express.json());
+
+//define schema for snippet routes
+
+snippetSchema = joi.object().keys({
+  title: joi.string().empty(""),
+  note: joi.string().empty(""),
+  snippet: joi.string().required(),
+  language_id: joi.number().required(),
+  user_id: joi.number().required(),
+});
 
 router.get("/api/user/snippets", authenticateToken, (req, res) => {
   let user_id = req.user.id;
@@ -37,73 +48,73 @@ router.get("/api/user/snippets/:id", authenticateToken, (req, res) => {
 });
 
 router.post("/api/user/snippets", authenticateToken, (req, res) => {
+  // assigning the user id to the req body
   req.body.user_id = req.user.id;
 
-  // validation
-  if (req.body.snippet === "" || !req.body.snippet) {
-    return res.status(400).json({
-      error: "Must provide a code snippet",
-    });
-  }
-
-  if (!req.body.user_id || !req.body.language_id) {
-    return res.status(400).json({
-      error: "Must provide a user id and language id",
-    });
-  }
-
   // create snippet object for database
-  let snippet = req.body;
+  const snippet = req.body;
 
-  db.snippet
-    .create(snippet)
-    .then((data) => {
-      return res.status(200).json({ snippet: data });
-    })
-    .catch((err) => {
-      res.status(400).json({
-        message: err.parent.detail || "Some error occured",
-      });
+  const result = snippetSchema.validate(snippet);
+
+  const { value, error } = result;
+
+  const valid = error == null;
+  console.log(error);
+  if (!valid) {
+    res.status(400).json({
+      error: "Invalid request",
     });
+  } else {
+    db.snippet
+      .create(snippet)
+      .then((data) => {
+        return res.status(200).json({ snippet: data });
+      })
+      .catch((err) => {
+        res.status(400).json({
+          message: err.parent.detail || "Some error occured",
+        });
+      });
+  }
 });
 
 router.put("/api/user/snippets/:id", authenticateToken, (req, res) => {
+  // assigning the user id to the req body
+
   let snippet_id = req.params.id;
 
-  // validation
-  if (req.body.snippet === "" || !req.body.snippet) {
-    return res.status(400).json({
-      error: "Must provide a code snippet",
-    });
-  }
-  if (!req.body.user_id || !req.body.language_id) {
-    return res.status(400).json({
-      error: "Must provide a user id and language id",
-    });
-  }
+  const snippet = req.body;
 
-  // create snippet object for database
-  let snippet = req.body;
+  const result = snippetSchema.validate(snippet);
 
-  db.snippet
-    .update(snippet, {
-      where: { id: snippet_id },
-    })
-    .then((data) => {
-      console.log(data);
-      if (data[0] === 1) {
-        return res.status(200).json({ success: "Snippet updated!" });
-      } else {
-        return res
-          .status(200)
-          .json({ error: `Unable to update snippet with id of ${snippet_id}` });
-      }
-    })
-    .catch((err) => {
-      res.status(500).json({
-        message: err.parent.detail || "Some error occured",
+  const { value, error } = result;
+
+  const valid = error == null;
+  if (!valid) {
+    res.status(400).json({
+      error: "Invalid request",
+    });
+  } else {
+    db.snippet
+      .update(snippet, {
+        where: { id: snippet_id },
+      })
+      .then((data) => {
+        console.log(data);
+        if (data[0] === 1) {
+          return res.status(200).json({ success: "Snippet updated!" });
+        } else {
+          return res.status(200).json({
+            error: `Unable to update snippet with id of ${snippet_id}`,
+          });
+        }
+      })
+      .catch((err) => {
+        res.status(500).json({
+          message: err.parent.detail || "Some error occured",
+        });
       });
-    });
+  }
 });
 
 router.delete("/api/user/snippets/:id", authenticateToken, (req, res) => {
